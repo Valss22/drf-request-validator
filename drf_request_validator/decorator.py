e28fd9880a2deb70e6c712a15af0fd1c6bf63743
vladislavshokorov <valsshokorov@gmail.com>
2022-12-30 14:56:19 +0600

    Make more detailed validation errors response

--- drf_request_validator/decorator.py
+++ drf_request_validator/decorator.py
@@ -1,5 +1,14 @@
 from rest_framework.request import Request
 from rest_framework.response import Response
+from typing import TypedDict
+
+
+class RequestBodyError(TypedDict):
+    key_errors: list[str]
+    type_errors: list[str]
+
+
+detailed_errors: RequestBodyError = {}
 
 
 def request_validator(schema: dict):
@@ -9,20 +18,21 @@ def request_validator(schema: dict):
                 schema.items(), request.data.items()
             ):
                 if schema_key != data_key:
-                    return Response(
-                        {
-                            "error": "Request body key error",
-                            # "detail": f"Expected {schema_key}, but recieved {data_key}",
-                        }
+                    detailed_errors.update(
+                        detailed_errors["key_errors"].append(
+                            f"Field '{schema_key}' is expected, but '{data_key}' is recieved"
+                        )
                     )
                 if schema_type_key is not type(data_value):
-                    return Response(
-                        {
-                            "error": "Request body type error",
-                            # "detail": f"Expected {schema_type_key}"
-                            # + f"but recieved {type(data_value)}",
-                        }
+                    detailed_errors.update(
+                        detailed_errors["type_errors"].append(
+                            f"It's expected the field '{schema_key}'"
+                            + f"has the type {schema_type_key},"
+                            + f"but recieved {type(data_value)}"
+                        )
                     )
+            if detailed_errors:
+                return Response(detailed_errors)
             return func(request)
 
         return inner
