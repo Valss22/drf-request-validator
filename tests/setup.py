ea76978b045f219c7461687a9cf336990682eac5
vladislavshokorov <valsshokorov@gmail.com>
2023-01-04 15:31:26 +0600

    Optimize code syntax

--- tests/setup.py
+++ tests/setup.py
@@ -1,29 +1,32 @@
 from drf_request_validator.types import ErrorMessage
 
 
+def invalid_key_response(key: str) -> dict:
+    return {"key": key, "msg": ErrorMessage.INVALID_KEY}
+
+
+def invalid_type_response(key: str, expected_key_type) -> dict:
+    return {
+        "key": key,
+        "msg": ErrorMessage.INVALID_TYPE,
+        "detail": f"type {expected_key_type} is expected",
+    }
+
+
 schema = {"name": str, "age": int}
 
-correct_request_data = {"name": "Vlad", "age": 20}
-correct_response = {**correct_request_data}
-
-invalid_key_request_data = {"naame": "Vlad", "age": 20}
-invalid_key_response = [{"key": "naame", "msg": ErrorMessage.INVALID_KEY}]
-
-invalid_type_request_data = {"name": "Vlad", "age": "twenty"}
-invalid_type_response = [
-    {
-        "key": "age",
-        "msg": ErrorMessage.INVALID_TYPE,
-        "detail": f"type {int} is expected",
-    }
-]
-
-invalid_key_and_type_request_data = {"naame": "Danila", "age": "twenty"}
-invalid_key_and_type_response = [invalid_key_response[0], invalid_type_response[0]]
-
 test_data = [
-    (schema, correct_request_data, correct_response),
-    (schema, invalid_key_request_data, invalid_key_response),
-    (schema, invalid_type_request_data, invalid_type_response),
-    (schema, invalid_key_and_type_request_data, invalid_key_and_type_response),
+    (schema, {"name": "Vlad", "age": 20}, {"name": "Vlad", "age": 20}),
+    (schema, {"naame": "Rudolf", "age": 21}, [invalid_key_response("naame")]),
+    (schema, {"name": "Ruslan", "age": "twenty"}, [invalid_type_response("age", int)]),
+    (
+        schema,
+        {"naame": "Danila", "age": "twenty"},
+        [invalid_key_response("naame"), invalid_type_response("age", int)],
+    ),
+    (
+        schema,
+        {"naame": "Sergej", "agge": 20},
+        [invalid_key_response("naame"), invalid_key_response("agge")],
+    ),
 ]
