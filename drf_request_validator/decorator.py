4b9e6a66d1539fe2be9848312ed72f1fa6add2e4
Valss22 <valsshokorov@gmail.com>
2023-01-07 23:29:06 +0600

    Add simple missing key checking

--- drf_request_validator/decorator.py
+++ drf_request_validator/decorator.py
@@ -3,6 +3,19 @@ from rest_framework.response import Response
 from drf_request_validator.types import ErrorMessage, ErrorDetail
 
 
+# def missing_key_validation(req_data: dict, schema_data: dict, error_details: list):
+#     invalid_keys = [error_detail["key"] for error_detail in error_details]
+#     print(invalid_keys)
+#     for schema_key in schema_data.keys():
+#         if schema_key not in req_data.keys() and schema_key not in invalid_keys:
+#             error_details.append(
+#                 ErrorDetail(
+#                     msg=ErrorMessage.MISSING_KEY,
+#                     detail=f"key '{schema_key}' is missing",
+#                 )
+#             )
+
+
 def list_validation(req_data: list, schema_data: list, error_details: list):
     for req_list_item in req_data:
         if type(schema_data[0]) is dict:
@@ -34,9 +47,12 @@ def list_validation(req_data: list, schema_data: list, error_details: list):
 
 
 def dict_validation(req_data: dict, schema_data: dict, error_details: list):
+
     for req_key, req_value in req_data.items():
+
         if req_key not in list(schema_data.keys()):
             error_details.append(ErrorDetail(key=req_key, msg=ErrorMessage.INVALID_KEY))
+
         else:
             if type(schema_data[req_key]) is dict:
                 if type(req_data[req_key]) is dict:
@@ -85,10 +101,14 @@ def request_validator(schema: dict | list):
         def inner(request: Request):
 
             error_details: list[ErrorDetail] = []
+
             if type(request.data) is list and type(schema) is list:
                 list_validation(request.data, schema, error_details)
 
             elif type(request.data) is dict and type(schema) is dict:
+                if len(request.data) < len(schema):
+                    error_details.append(ErrorDetail(msg=ErrorMessage.MISSING_KEY))
+
                 dict_validation(request.data, schema, error_details)
 
             else:
